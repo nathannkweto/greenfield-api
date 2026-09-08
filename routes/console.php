d998714b593c151da90581bb0d06d8ae54882936
Nathan Nkweto <nathan@nkweto.com>
2026-09-08 11:34:43 +0100

    feat-schedule: add queue worker scheduling

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+
+Schedule::command('queue:work --stop-when-empty')
+    ->everyMinute()
+    ->withoutOverlapping();
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
